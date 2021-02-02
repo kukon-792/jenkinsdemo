b92e9ed42cbf914b797fe0551c0f3dfa287e0b52
kukon miah <kukon11674@gmail.com>
2021-02-02 16:56:09 -0500

    Demo

---
+++ src/main/java/jenkins/Test0.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello selenium",true);
+	}
+
+}
